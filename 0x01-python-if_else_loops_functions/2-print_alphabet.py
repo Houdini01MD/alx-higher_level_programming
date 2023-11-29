d29f1bdb0c6443b01c2babe9500862360bfb46d9
Houdini01MD <adjhoudini@gmail.com>
2023-11-29 23:57:26 +0100

    print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-startVal = ord('a')
-endVal = ord('z')
-
-for i in range(startVal, endVal + 1):
-    print(chr(i), end='')
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
