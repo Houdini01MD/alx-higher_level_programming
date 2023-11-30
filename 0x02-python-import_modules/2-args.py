e6c1079565c9f86253bd5b0b54096c6795f39f35
Houdini01MD <adjhoudini@gmail.com>
2023-11-30 19:10:58 +0100

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     """print number and value of argument"""
-
-num_arg = len(argv) - 1
+    num_arg = len(argv) - 1
 
 if num_arg == 1:
     print("1 argument:")
@@ -16,5 +15,5 @@ elif num_arg == 0:
 else:
     print("{} argument:".format(num_arg))
 
-for i in range(num_arg):
-    print("{}: {}".format(i + 1, argv[i + 1]))
+for i, arg in enumerate(argv[1:], start=1):
+    print("{}: {}".format(i, arg))
