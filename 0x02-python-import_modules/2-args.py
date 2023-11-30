6387adf32b6d8ef20fe901cc89d8248db57ef4ee
Houdini01MD <adjhoudini@gmail.com>
2023-11-30 19:12:20 +0100

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-from sys import argv
-
 if __name__ == "__main__":
     """print number and value of argument"""
-    num_arg = len(argv) - 1
+
+from sys import argv
+
+num_arg = len(argv) - 1
 
 if num_arg == 1:
     print("1 argument:")
