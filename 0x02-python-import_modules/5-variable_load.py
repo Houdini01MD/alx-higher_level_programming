3a52af5b8d5606a3d2518489f5dbe944740b80d7
Houdini01MD <adjhoudini@gmail.com>
2023-11-30 21:24:08 +0100

    variable load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """mports the variable a from the file variable_load_5.py and prints its value."""
+    """local variabl"""
 
 from variable_load_5 import a
 print(a)
