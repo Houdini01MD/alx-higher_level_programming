ab405d24072d90d7f0d4cc348eb535bb27a13423
Houdini01MD <adjhoudini@gmail.com>
2023-11-30 19:08:52 +0100

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -16,5 +16,5 @@ elif num_arg == 0:
 else:
     print("{} argument:".format(num_arg))
 
-for i, arg in enumerate(argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+for i in range(num_arg):
+    print("{}: {}".format(i + 1, argv[i + 1]))
