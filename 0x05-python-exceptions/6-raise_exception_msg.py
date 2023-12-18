e49e794f62bb2a7dc03fe4867190c89230b1124e
Houdini01MD <adjhoudini@gmail.com>
2023-12-18 23:41:31 +0100

    add raise_exception_msg function

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """raises a name exception with a message.
+
+    Args:
+        message: string
+    
+    Return:
+        nessage
+    """
+    raise NameError(message)
