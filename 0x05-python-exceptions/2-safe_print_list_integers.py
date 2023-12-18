7ba69f4c13d11d852b8f0ce9e8064b3f3eb8fad0
Houdini01MD <adjhoudini@gmail.com>
2023-12-18 18:32:56 +0100

    add safe_print_list_integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+    except:
+        pass
+    else:
+        print()
+        return count
