dd23de5c5d9ebcb58584012728008e5f9cb9ef3a
Houdini01MD <adjhoudini@gmail.com>
2023-12-18 23:35:01 +0100

    add raise_exception function

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """divides element by element 2 lists.
+
+    Args:
+        my_list_1: firdt list
+        my_list_2: second list
+        list_length: The number of elements to divide.
+
+    Return:
+        a new list (length = list_length) with all divisions
+    """
+    raise TypeError("this is a type exception")
