737eba2978fc54e521441b9d0d108d75403c6c2c
Houdini01MD <ayoubelattr@gmail.com>
2024-01-04 22:56:14 +0100

    task 1. Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,14 +3,17 @@
 
 
 class Rectangle:
+    """Rectangle class"""
     def __init__(self, width=0, height=0):
-        """class Rectangle.
+        """Rectangle constructor.
+
+
         Args:
             with : the width of rectangle
             height : the height of rectangle
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
         @property
         def width(self):
@@ -19,9 +22,9 @@ class Rectangle:
 
         @width.setter
         def width(self, value):
-                if type(value) is not int:
+                if not isinstance(value, int):
                     raise TypeError("width must be an integer")
-                elif value < 0:
+                if value < 0:
                     raise ValueError("width must be >= 0")
                 self.__width = value
 
@@ -30,10 +33,10 @@ class Rectangle:
             """get/set the height of rectangle"""
             return self.__width
 
-        @width.setter
+        @height.setter
         def height(self, value):
-                if type(value) is not int:
+                if not isinstance(value, int):
                     raise TypeError("height must be an integer")
-                elif value < 0:
+                if value < 0:
                     raise ValueError("height must be >= 0")
                 self.__height = value
