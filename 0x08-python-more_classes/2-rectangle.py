1317a3ca0bf89d3f42bfc178874b33deaa1a6145
Houdini01MD <ayoubelattr@gmail.com>
2024-01-05 02:20:35 +0100

    task 2 Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
 
     def perimeter(self):
         """calcul rectangle perimeter : 2*(width + height)"""
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__width + self.__height)
